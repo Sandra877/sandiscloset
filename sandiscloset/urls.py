e7a13e86143e79f791e08226807bf08ac654cae0
Sandra877 <110770016+Sandra877@users.noreply.github.com>
2024-11-06 22:12:32 +0300

    addedproductsapp

--- sandiscloset/urls.py
+++ sandiscloset/urls.py
@@ -17,9 +17,17 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('sandiscloset/', include('users.urls')),  # Include your users app URLs
+    path('products/', include('products.urls')),
+
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
